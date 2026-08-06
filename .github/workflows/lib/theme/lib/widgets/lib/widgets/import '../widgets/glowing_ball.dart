04c9878088f19394b2cf794ede41469773959191
ransari6442-lab <ransari6442@gmail.com>
2026-08-07 00:02:25 +0530

    Create glowing_ball.dart

---
+++ .github/workflows/lib/theme/lib/widgets/lib/widgets/import '../widgets/glowing_ball.dart
@@ -0,0 +1,8 @@
+Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: const [
+    GlowingBall(number: "2", size: 80),
+    GlowingBall(number: "2", size: 110),
+    GlowingBall(number: "2", size: 80),
+  ],
+),
